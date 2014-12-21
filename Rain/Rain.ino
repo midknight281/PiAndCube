b352f53c36ccd52b3d6df633cc1b580f44af2cfd
Stephen <sneeling@gmail.com>
2014-12-21 06:57:38 +0000

    Added more rain

--- Rain/Rain.ino
+++ Rain/Rain.ino
@@ -6,9 +6,10 @@
 */
 
 #include <Rainbowduino.h>
-int randomposition = random(12);
-int oldposition = random(12);
-int timedelay = 25;// Will tell weather it is heavy or light rain.
+int randomposition = random(16);
+int midposition = random(16);
+int oldposition = random(16);
+int timedelay = 50;// Will tell weather it is heavy or light rain.
 
 void setup()
 {
@@ -19,16 +20,16 @@ unsigned int z,x,y;
 
 void loop()
 {
- for(z=3;z>0;z--)
+ for(z=3;z<-1;z--)
  { 
      Rb.setPixelZXY(z,abs(randomposition/4),(randomposition%4),0x0000FF); //uses 24bit color mode
+     Rb.setPixelZXY(z,abs(midposition/4),(midposition%4),0x000044); //uses 24bit color mode
      Rb.setPixelZXY(z,abs(oldposition/4),(oldposition%4),0x000000); //uses 24bit color mode     
      delay(timedelay);
     }
-  Rb.setPixelZXY(z,abs(randomposition/4),(randomposition%4),0x0000FF); //uses 24bit color mode
-  Rb.setPixelZXY(z,abs(oldposition/4),(oldposition%4),0x000000); //uses 24bit color mode     
-  oldposition = randomposition;
-  randomposition = random(12);
+  oldposition = midposition;
+  midposition = randomposition;
+  randomposition = random(16);
 }
 /*
 Will use this later for a timer to shut off cube.
