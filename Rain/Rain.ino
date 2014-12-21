86d73d9651554a6f58ac5355ae45c779aea853eb
Stephen <sneeling@gmail.com>
2014-12-21 07:56:31 +0000

    Made rain a seperate procedure

--- Rain/Rain.ino
+++ Rain/Rain.ino
@@ -6,32 +6,42 @@
 */
 
 #include <Rainbowduino.h>
-int randomposition = random(16);
-int midposition = random(16);
-int oldposition = random(16);
-int timedelay = 50;// Will tell weather it is heavy or light rain.
+
 
 void setup()
 {
   Rb.init(); //initialize Rainbowduino driver
+  
 }
 
 unsigned int z,x,y;
 
 void loop()
 {
- for(z=3;z<-1;z--)
- { 
-     Rb.setPixelZXY(z,abs(randomposition/4),(randomposition%4),0x0000FF); //uses 24bit color mode
-     Rb.setPixelZXY(z,abs(midposition/4),(midposition%4),0x000044); //uses 24bit color mode
-     Rb.setPixelZXY(z,abs(oldposition/4),(oldposition%4),0x000000); //uses 24bit color mode     
+
+  ShowItRaining(50,0,66);
+}
+
+
+void ShowItRaining(int speed,int lightning,int temperature)
+{
+  int rainDropPosition[3] = {random(16),random(16),random(16)};
+  int timedelay = 50;// Will tell weather it is heavy or light rain.
+  int numberOfIterations = 20000;
+  int x;
+  for (x=1;x<numberOfIterations;x++)
+  {
+  for(z=3;z<-1;z--)
+    { 
+     Rb.setPixelZXY(z,abs(rainDropPosition[0]/4),(rainDropPosition[0]%4),0x0000FF); //uses 24bit color mode
+     Rb.setPixelZXY(z,abs(rainDropPosition[1]/4),(rainDropPosition[1]%4),0x000038); //uses 24bit color mode
+     Rb.setPixelZXY(z,abs(rainDropPosition[2]/4),(rainDropPosition[2]%4),0x000000); //uses 24bit color mode
      delay(timedelay);
     }
-  oldposition = midposition;
-  midposition = randomposition;
-  randomposition = random(16);
+  rainDropPosition[2] = rainDropPosition[1];
+  rainDropPosition[1] = rainDropPosition[0];
+  rainDropPosition[0]= random(16);
+  }  
+  Rb.blankDisplay(); //Clear the LEDs (make all blank)
 }
-/*
-Will use this later for a timer to shut off cube.
-Rb.blankDisplay(); //Clear the LEDs (make all blank)
-*/
+
